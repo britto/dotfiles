8ec6762657e4f64ae4f93ecc6c76ee408fe28afa
João Britto <jabcalves@gmail.com>
2013-08-11 15:52:19 -0300

    Do not force delete

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 for file in ~/.dotfiles/dotfiles/*; do
   echo "Installing $(basename $file)..."
-  rm -rf ~/.$(basename $file)
+  rm -r ~/.$(basename $file)
   ln -s $file ~/.$(basename $file)
 done
