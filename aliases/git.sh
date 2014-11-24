5a782d13b44337b4e443e1f2c81c8e3d753cfbb5
João Britto <jabcalves@gmail.com>
2014-11-24 13:56:47 -0200

    Git aliases overhaul

--- aliases/git.sh
+++ aliases/git.sh
@@ -1,30 +1,16 @@
+# Shorter alias for git with autocomplete support
 alias g='git'
-
 complete -o bashdefault -o default -o nospace -F _git g $1 2>/dev/null || complete -o default -o nospace -F _git g $1
 
-alias gdf='git diff --minimal'
-alias gdfs='gdf --staged'
-alias gd='gdf --word-diff=color'
-alias gds='gd --staged'
-
+# General aliases for the most used commands
+# TODO reduce these and get used to their real git alias counterparts
 alias ga='git add -A'
-alias gap='ga -p'
+alias gs='git status -sb'
+alias gpl='git pull'
+alias gps='git push'
 
-alias gb='git branch -vv'
-
-alias gk='gitk --all &'
-
-alias gco='git checkout'
-alias gci='git commit'
-alias gs='git status'
-alias glc='git log --decorate --graph --oneline --abbrev-commit'
-alias gl='glc --all'
-
+# Stash related aliases
 alias gsl='git stash list'
 alias gss='git stash save -u --keep-index'
 alias gsp='git stash pop'
 alias gsd='git stash drop'
-
-alias gpl='git pull'
-alias gps='git push'
-alias gboom='git reset --hard; git clean -df'
