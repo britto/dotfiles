26e5752d82588a689d74af6712a30f09b1939d90
João Britto <jabcalves@gmail.com>
2016-07-04 15:23:10 -0300

    Get rid of more bash aliases in favor of git aliases

--- aliases/git.sh
+++ aliases/git.sh
@@ -2,14 +2,6 @@
 alias g='git'
 complete -o bashdefault -o default -o nospace -F _git g $1 2>/dev/null || complete -o default -o nospace -F _git g $1
 
-# General aliases for the most used commands
-# TODO reduce these and get used to their real git alias counterparts
 alias ga='git a'
 alias gs='git status -sb'
 alias gl='git l'
-
-# Stash related aliases
-alias gsl='git stash list'
-alias gss='git stash save -u --keep-index'
-alias gsp='git stash pop'
-alias gsd='git stash drop'
