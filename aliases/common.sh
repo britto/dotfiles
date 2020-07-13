35d9b8cb468e77df5cc02ffc4a25fc345dd19a81
João Britto <jabcalves@gmail.com>
2020-07-12 21:36:00 -0500

    Disable some unused aliases

--- aliases/common.sh
+++ aliases/common.sh
@@ -5,19 +5,19 @@ alias ll='l -l'
 alias lla='la -l'
 
 # Rubygems
-alias gemdup='gem list | grep ","'
-alias gemempty='gem list | cut -d" " -f1 | xargs gem uninstall -aIx'
+# alias gemdup='gem list | grep ","'
+# alias gemempty='gem list | cut -d" " -f1 | xargs gem uninstall -aIx'
 
 # Finder
-alias reveal='open -R'
-alias preview='open . -a Preview'
-alias ql='qlmanage -p "$@" >& /dev/null'
-alias cleanup_open_with_menu='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
+# alias reveal='open -R'
+# alias preview='open . -a Preview'
+# alias ql='qlmanage -p "$@" >& /dev/null'
+# alias cleanup_open_with_menu='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
 
 # Utils
 alias internet='ping -ni8 8.8.8.8 2>&1 | grep "bytes from"'
 alias download='curl -LO'
-alias serve='ruby -run -e httpd . -p80'
+# alias serve='ruby -run -e httpd . -p80'
 alias alert='tput bel'
 alias notify='osascript -e "display notification \"Stuff is done.\" with title \"Back to work!\""'
 alias ls_by_size='du -s */ | sort -nr | cut -d"	" -f2 | while read -r x; do du -sh "$x"; done'
@@ -26,8 +26,8 @@ alias xml_pretty='xmllint --format -'
 alias screen_resolution='system_profiler SPDisplaysDataType | grep Resolution'
 
 # Media
-alias mp3_split='mp3splt -s -p rm'
-alias jpg_optimize='find . -name "*.jpg" -type f -exec jpegtran -copy none -optimise -outfile {} {} \;'
+# alias mp3_split='mp3splt -s -p rm'
+# alias jpg_optimize='find . -name "*.jpg" -type f -exec jpegtran -copy none -optimise -outfile {} {} \;'
 
 # Typos
-alias brwe='brew'
+# alias brwe='brew'
--- aliases/git.sh
+++ aliases/git.sh
@@ -1,6 +1,6 @@
 # Shorter alias for git with autocomplete support
 alias g='git'
-complete -o bashdefault -o default -o nospace -F _git g $1 2>/dev/null || complete -o default -o nospace -F _git g $1
+# complete -o bashdefault -o default -o nospace -F _git g $1 2>/dev/null || complete -o default -o nospace -F _git g $1
 
 alias ga='git a'
 alias gs='git status -sb'
