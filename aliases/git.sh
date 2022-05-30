7478c593e63c5041470711141cf69ee7743a50b7
João Britto <jabcalves@gmail.com>
2022-05-30 14:26:14 -0300

    Update git settings

--- aliases/git.sh
+++ aliases/git.sh
@@ -1,7 +1,8 @@
 # Shorter alias for git with autocomplete support
-alias g='git'
+# alias g='git'
 # complete -o bashdefault -o default -o nospace -F _git g $1 2>/dev/null || complete -o default -o nospace -F _git g $1
 
-alias ga='git a'
-alias gs='git status -sb'
-alias gl='git l'
+# A few remappings of oh-my-zsh defaults
+alias ga='gaa'
+alias gs='gsb'
+alias gl='gloga'
